8d96d0a7691c7f93a6917daba6bde88cf655f395
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2017-09-14 09:25:04 +0200

    update setup to indicate 3.6

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     entry_points={
         'console_scripts': [
