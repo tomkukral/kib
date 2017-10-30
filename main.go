b59b2758523c769c5d3fe498e2683d78895564a7
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2017-10-30 22:13:48 +0100

    don't export getClientConfig

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-func GetClientConfig(kubeconfig string) (*rest.Config, error) {
+func getClientConfig(kubeconfig string) (*rest.Config, error) {
 	if kubeconfig != "" {
 		return clientcmd.BuildConfigFromFlags("", kubeconfig)
 	}
@@ -29,7 +29,7 @@ func main() {
 	password := flag.String("password", "", "Password for Docker Registry")
 	flag.Parse()
 
-	config, err := GetClientConfig(*kubeconf)
+	config, err := getClientConfig(*kubeconf)
 	if err != nil {
 		panic(err.Error())
 	}
